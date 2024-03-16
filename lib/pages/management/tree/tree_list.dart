e8925124dae1aa12310d7320bcc3016eb89b55b1
Firdaus <firdauskhamid@student.usm.my>
2024-03-17 03:43:01 +0800

    Sorting and filtering list of trees

--- lib/pages/management/tree/tree_list.dart
+++ lib/pages/management/tree/tree_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:sawitcare_app/main.dart';
 import 'package:sawitcare_app/pages/management/tree/tree_list_widget.dart';
 import 'package:sawitcare_app/pages/management/tree/tree_screen.dart';
@@ -14,6 +15,12 @@ class TreeList extends StatefulWidget {
 class _TreeListState extends State<TreeList> {
   List? _treeList;
   bool _isLoading = true; // Initialize as true to show loading initially
+  List? _originalTreeList; // Store original unfiltered list
+  bool _sortByDescending = true; // Added for sorting
+  bool _filterByBlock = false; // Added for filtering
+  String _selectedBlock = 'Block'; // Added for filtering
+  String _selectedSortOption = 'Sort by'; // Added for sorting
+  List<String> _blockTypes = ['Block']; // Initialize with 'Block'
 
   @override
   void initState() {
@@ -45,38 +52,144 @@ class _TreeListState extends State<TreeList> {
           ],
         ),
       ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Center(
-              child: ListView.separated(
-                separatorBuilder: (context, index) => const Divider(
-                  height: 1, // Adjust separator height as needed
-                  color: Color.fromARGB(
-                      255, 218, 218, 218), // Customize separator color
-                ),
-                itemCount: _treeList?.length ?? 0,
-                itemBuilder: (context, index) {
-                  return Container(
-                    decoration: (BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-                    height: 75,
-                    child: // Tree List Widget
-                        TreeListWidget(
-                      treeId: _treeList![index]['block'].toString() +
-                          _treeList![index]['tree_number'].toString(),
-                      harvest: _treeList![index]['daysHarvested'],
-                      fertilize: _treeList![index]['daysFertilized'],
-                      prune: _treeList![index]['daysPruned'],
-                    ),
-                  );
+      body: Column(
+        children: [
+          // Sorting and Filtering Widgets
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              DropdownButton<String>(
+                value: _selectedBlock, // Default value
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      _selectedBlock =
+                          newValue; // Update selected sorting option
+                      _filterTreeListByBlock(newValue);
+                    });
+                  }
                 },
+                items:
+                    _blockTypes.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text('$value'),
+                  );
+                }).toList(),
               ),
-            ),
+              DropdownButton<String>(
+                value: _selectedSortOption, // Default value
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      _selectedSortOption =
+                          newValue; // Update selected sorting option
+                    });
+                  }
+                },
+                items: <String>['Sort by', 'Harvested', 'Pruned', 'Fertilized']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text('$value'),
+                  );
+                }).toList(),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _sortByDescending = !_sortByDescending;
+                    _sortTreeList(_selectedSortOption);
+                  });
+                },
+                child: Row(
+                  children: [
+                    _sortByDescending
+                        ? const Icon(Icons
+                            .keyboard_arrow_up_rounded) // Icon for descending order
+                        : const Icon(Icons
+                            .keyboard_arrow_down_rounded), // Icon for ascending order
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: _isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Center(
+                    child: ListView.builder(
+                      itemCount: _treeList?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.all(5),
+                          decoration: (BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          )),
+                          height: 75,
+                          child: // Tree List Widget
+                              TreeListWidget(
+                            treeId: _treeList![index]['block'].toString() +
+                                _treeList![index]['tree_number'].toString(),
+                            harvest: _treeList![index]['daysHarvested'],
+                            fertilize: _treeList![index]['daysFertilized'],
+                            prune: _treeList![index]['daysPruned'],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _sortTreeList(String? sortOption) {
+    if (_sortByDescending) {
+      _treeList?.sort((a, b) {
+        // Handle null values by putting them at the bottom
+        if (a['days$sortOption'] == null && b['days$sortOption'] == null) {
+          return 0;
+        } else if (a['days$sortOption'] == null) {
+          return 1;
+        } else if (b['days$sortOption'] == null) {
+          return -1;
+        } else {
+          return b['days$sortOption'].compareTo(a['days$sortOption']);
+        }
+      });
+    } else {
+      _treeList?.sort((a, b) {
+        // Handle null values by putting them at the bottom
+        if (a['days$sortOption'] == null && b['days$sortOption'] == null) {
+          return 0;
+        } else if (a['days$sortOption'] == null) {
+          return 1;
+        } else if (b['days$sortOption'] == null) {
+          return -1;
+        } else {
+          return a['days$sortOption'].compareTo(b['days$sortOption']);
+        }
+      });
+    }
+  }
+
+  void _filterTreeListByBlock(String blockType) {
+    setState(() {
+      if (blockType == 'Block') {
+        // If 'Block' is selected, show all trees
+        _treeList = _originalTreeList;
+      } else {
+        // Filter the tree list to contain only trees with the selected block type
+        _treeList = _originalTreeList
+            ?.where((tree) => tree['block'] == blockType)
+            .toList();
+      }
+    });
   }
 
   Future<void> _fetchTreeList() async {
@@ -87,7 +200,18 @@ class _TreeListState extends State<TreeList> {
       final data = await fetchTreeList(); // Call the service function
       setState(() {
         _treeList = data ?? []; // Assign fetched data to _treeList
-        _isLoading = false; // Set loading to false after fetching
+        _blockTypes = ['Block']; // Reset the list before updating
+        _originalTreeList = data ?? []; // Store the original unfiltered list
+        // Extract unique block types
+        _treeList?.forEach((tree) {
+          if (!_blockTypes.contains(tree['block'].toString())) {
+            _blockTypes.add(tree['block'].toString());
+          }
+        });
+        // Sort the tree list immediately after fetching
+        _sortTreeList(_selectedSortOption);
+        _isLoading = false;
+        // Set loading to false after fetching
       });
       print(_treeList);
     } catch (e) {
--- lib/pages/management/tree/tree_list_widget.dart
+++ lib/pages/management/tree/tree_list_widget.dart
@@ -18,68 +18,56 @@ class TreeListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
-          child: Card(
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            color: const Color(0x6639D2C0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.all(8),
-              child: Icon(
-                Icons.forest,
-                size: 24,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.grey.shade300, width: 2),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(12, 12, 0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Tree $treeId',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(12, 25, 0, 0),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
             child: Column(
               mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: Text(
-                    'Tree $treeId',
-                    // '#${treeId}',
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
+                Text(harvest != null
+                    ? 'Harvested $harvest days ago'
+                    : 'Harvesting data unavailable'),
+                Text(prune != null
+                    ? 'Pruned $prune days ago'
+                    : 'Pruning data unavailable'),
+                Text(fertilize != null
+                    ? 'Fertilized $fertilize days ago'
+                    : 'Fertilizing data unavailable'),
               ],
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsetsDirectional.fromSTEB(12, 0, 12, 0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Text(harvest != null
-                  ? 'Harvested $harvest days ago'
-                  : 'Harvesting data unavailable'),
-              Text(prune != null
-                  ? 'Pruned $prune days ago'
-                  : 'Pruning data unavailable'),
-              Text(fertilize != null
-                  ? 'Fertilized $fertilize days ago'
-                  : 'Fertilizing data unavailable'),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
